44d0c2e60e973f96c066179bd03c064a807f7392
abdel-ely-ds <abdel.ely.ds@gmail.com>
2022-06-15 21:03:06 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+from setuptools import setup
+
+TEST_DEPS = ["pytest==5.0.1", "pytest-runner==5.1", "pytest-cov==2.7.1", "nox"]
+
+with open("requirements.txt") as f:
+    requirements = f.readlines()
+
+setup(
+    name="drugs-price-predictor",
+    version="0.1.0",
+    description="Predicting drugs prices",
+    keywords=["regression", "drugs"],
+    author="abdel.ely.ds",
+    license="MIT",
+    classifiers=["Programming Language :: Python :: 3.7"],
+    zip_safe=True,
+    include_package_data=True,
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
+    entry_points={"console_scripts": ["drugs=drugs.cli.cli:main"]},
+    install_requires=requirements,
+    tests_require=TEST_DEPS,
+    extras_require={"test": TEST_DEPS},
+)
